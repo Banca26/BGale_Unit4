32e84f19eb83b95f900ab32b5bebb9c635c9de28
Banca26 <bia.henri@hotmail.com>
2022-04-19 14:18:04 -0700

    Lesson C Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     GameObject focalPoint;
     Renderer rendererPlayer;
     public float speed = 10.0f;
+    public float powerUpSpeed = 10.0f;
+    public GameObject powerUpInd;
+
+    bool hasPowerUp = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,8 +29,8 @@ public class PlayerController : MonoBehaviour
         float magnitude = forwardInput * speed * Time.deltaTime;
         rbPlayer.AddForce(transform.forward * magnitude, ForceMode.Force);
 
-        Debug.Log("Mag:" + magnitude);
-        Debug.Log("FI:" + forwardInput);
+
+
 
         if (forwardInput > 0)
         {
@@ -36,5 +40,39 @@ public class PlayerController : MonoBehaviour
         {
             rendererPlayer.material.color = new Color(1.0f + forwardInput, 1.0f, 1.0f + forwardInput);
         }
+        powerUpInd.transform.position = transform.position;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if(other.CompareTag("PowerUp"))
+        {
+
+            hasPowerUp = true;
+            Destroy(other.gameObject);
+            StartCoroutine(PowerUpCountDown());
+            powerUpInd.SetActive(true);
+        } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (hasPowerUp && collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player has collid with" + collision.gameObject + " with powerup set to: " + hasPowerUp);
+            Rigidbody rbEnemy = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayDir = collision.gameObject.transform.position - transform.position;
+
+            rbEnemy.AddForce(awayDir * powerUpSpeed, ForceMode.Impulse);
+        }
+    }
+
+    IEnumerator PowerUpCountDown()
+    {
+        yield return new WaitForSeconds(8);
+        hasPowerUp = false;
+        powerUpInd.SetActive(false);
     }
 }
